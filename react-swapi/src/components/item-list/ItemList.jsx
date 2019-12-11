53e6ec79bb1aaa21ff8cbe72d41fe7667dc076b7
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-11 10:02:34 +0300

    Use API response in this component

--- react-swapi/src/components/item-list/ItemList.jsx
+++ react-swapi/src/components/item-list/ItemList.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ItemList.css";
+import SwapiService from "../../services/SwapiService";
+import Preloader from "../preloader/Preloader";
 
-const ItemList = () => {
+const ItemList = ({onItemSelected}) => {
+  const swapiServise = new SwapiService();
+  const [peopleList, setPeopleList] = useState(null);
+  useEffect(() => {
+    swapiServise.getAllPersons().then(peopleList => {
+      setPeopleList(peopleList);
+    });
+  }, []);
+
+  const renderItems = arr => {
+    return arr.map(({id, name}) => {
+      return <li className="list-group-item"
+        key={id}
+        onClick={() => onItemSelected(id)}>
+        {name}</li>;
+    });
+  };
+
+  if (!peopleList) {
+    return <Preloader />;
+  }
+  const items = renderItems(peopleList)
+
   return (
     <div>
       <ul className="item-list list-group">
-        <li className="list-group-item">Luke Skywalker</li>
-        <li className="list-group-item">Darth Vader</li>
-        <li className="list-group-item">R2-D2</li>
+        {items}
       </ul>
     </div>
   );
