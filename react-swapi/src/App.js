b1bee1496a9b7dbfc988c06c9c63e2e55be4dae8
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-13 08:35:05 +0300

    Using <PeoplePage /> instead of the previous piece of code

--- react-swapi/src/App.js
+++ react-swapi/src/App.js
@@ -2,19 +2,12 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './components/header/Header'
 import RandomPlanet from './components/random-planet/RandomPlanet'
-import { Switch, Route } from "react-router-dom";
-import PersonDetails from './components/person-details/PersonDetails'
-import PlanetDetails from './components/planet-details/PlanetDetails'
-import StarshipDetails from './components/starship-details/StarshipDetails'
-import NotFound from './components/not-found/NotFound'
-import ItemList from './components/item-list/ItemList'
+import PeoplePage from './components/people-page/PeoplePage';
+import SwapiService from './services/SwapiService'
 
 const App = () => {
-  const [selectedPerson, setSlectedPerson] = useState(null)
 
-  const onPersonSelected = (id) => {
-    setSlectedPerson(id)
-  }
+  const swapiServise = new SwapiService();
   return (
     <div className="wrapper">
       <header>
@@ -23,16 +16,11 @@ const App = () => {
       <div className='random-planet'>
         <RandomPlanet />
       </div>
-      <div className="items row mb2">
-        <div className="col-md-6">
-          <ItemList onItemSelected={onPersonSelected} />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails selectedPerson={selectedPerson} />
-        </div>
+      <div>
+        <PeoplePage />
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
