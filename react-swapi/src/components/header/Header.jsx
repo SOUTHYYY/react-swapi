48c390c36f0620704dc811b85385a30a8453f3bb
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-15 14:58:32 +0300

    Use NavLinks

--- react-swapi/src/components/header/Header.jsx
+++ react-swapi/src/components/header/Header.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import './Header.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <>
-          <div className="header d-flex">
-      <h3>
-        <NavLink to='/'>Star Wars API</NavLink>
-      </h3>
-      <ul className="d-flex">
-        <li>
-          <NavLink to='persons'>People</NavLink>
-        </li>
-        <li>
-          <NavLink to='planets'>Planets</NavLink>
-        </li>
-        <li>
-          <NavLink to='starships'>Starships</NavLink>
-        </li>
-      </ul>
-    </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="header d-flex">
+        <h3>
+          <NavLink to="/">Star Wars API</NavLink>
+        </h3>
+        <ul className="d-flex">
+          <li>
+            <NavLink to="/persons">People</NavLink>
+          </li>
+          <li>
+            <NavLink to="/planets">Planets</NavLink>
+          </li>
+          <li>
+            <NavLink to="/starships">Starships</NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
-
-export default Header
+export default Header;
