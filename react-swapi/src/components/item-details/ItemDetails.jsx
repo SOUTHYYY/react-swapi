65e879f6c75a8421851c5165f130e62c9396508d
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-15 15:16:47 +0300

    Reafactored Component
    Change func name from updatePerson to updateItem, now with the help of context, all the necessary data comes in props and we do not need to call swapiService in the component

--- react-swapi/src/components/item-details/ItemDetails.jsx
+++ react-swapi/src/components/item-details/ItemDetails.jsx
@@ -1,40 +1,44 @@
 import React, { useState, useEffect } from "react";
 import "./ItemDetails.css";
-import SwapiService from "../../services/SwapiService";
 import Preloader from "../preloader/Preloader";
 import search_image from "./search_img.png";
-import ViewPersonDetails from './view-details/WievDetails'
+import ViewPersonDetails from "./view-details/WievDetails";
 
-const ItemDetails = ({ selectedItem, getData, ...props }) => {
-  const swapiService = new SwapiService();
+const ItemDetails = ({ selectedItem, getImageUrl, getData, ...props }) => {
   const [item, setItem] = useState(null);
+  const [image, setImage] = useState(null);
   const [isFetching, setIsFetching] = useState(false);
-  const updatePerson = () => {
+
+  const updateItem = () => {
     if (!selectedItem) {
       return;
     }
     setIsFetching(true);
-    swapiService.getPerson(selectedItem).then(item => {
+    getData(selectedItem).then(item => {
       setIsFetching(false);
       setItem(item);
+      setImage(getImageUrl(item));
     });
   };
+
   useEffect(() => {
-    updatePerson();
+    updateItem();
   }, [selectedItem]);
 
   if (!item) {
     return (
       <div className="nothing-at-all">
         <span>Выберите персонажа из списка слева</span>
-        <img src={search_image} alt="search image" />
+        <img src={search_image} alt="search" />
       </div>
     );
   }
 
   const hasData = !(isFetching || !item);
 
-  const data = hasData ? <ViewPersonDetails item={item} {...props} /> : null;
+  const data = hasData ? (
+    <ViewPersonDetails item={item} {...props} image={image} />
+  ) : null;
   const loading = isFetching ? <Preloader /> : null;
   return (
     <div className="person-details card">
