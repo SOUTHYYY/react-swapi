df408d50ac8bae0b5ae522724612458d9fbb3d55
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-15 15:04:50 +0300

    Add isFetching and error in state

--- react-swapi/src/components/hoc/WithData.jsx
+++ react-swapi/src/components/hoc/WithData.jsx
@@ -1,24 +1,41 @@
-import React, { useState, useEffect, Component } from "react";
-import ErrorIndicator from "../random-planet/error-indicator/ErrorIndicator";
+import React, { Component } from "react";
 import Preloader from "../preloader/Preloader";
+import ErrorIndicator from "../random-planet/error-indicator/ErrorIndicator";
 
-const WithData = (View, getData) => {
+const WithData = View => {
   return class extends Component {
     state = {
-      datat: null
+      data: null,
+      isFetching: false,
+      error: false
     };
 
     componentDidMount() {
-      getData().then(data => {
-        this.setState({
-          data
-        });
+      this.setState({
+        isFetching: true,
+        error: false
       });
+      this.props.getData()
+      .then(data => {
+        this.setState({
+          data,
+          isFetching: false
+        });
+      })
+      .catch(() => {
+        this.setState({
+          isFetching: false,
+          error: true
+        })
+      })
     }
     render() {
-      const { data } = this.state;
-      if (!data) {
+      const { data, isFetching, error } = this.state;
+      if (!data || isFetching) {
         return <Preloader />;
+      }
+      if (error) {
+        return <ErrorIndicator />;
       }
       return <View {...this.props} data={data} />;
     }
